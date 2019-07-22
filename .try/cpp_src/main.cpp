47943a9a293624e0b3258eec5fe1a14f4d15a3eb
Sven Erdem <sven.erdem@kdab.com>
2019-07-22 12:48:20 +0200

    try cpp improvements

--- .try/cpp_src/main.cpp
+++ .try/cpp_src/main.cpp
@@ -1,4 +1,10 @@
+#include <type_traits>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+using namespace std;
+using namespace std::string_literals;
 
 int main(int argc, char* argv[])
 {

2cbc137e793ac278a1f06224c67440f81d423c3f
Sven Erdem <sven.erdem@kdab.com>
2019-10-02 14:25:59 +0200

    try cpp improved

--- .try/cpp_src/main.cpp
+++ .try/cpp_src/main.cpp
@@ -1,10 +1,15 @@
-#include <type_traits>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <functional>
 #include <iostream>
-#include <vector>
-#include <string>
 #include <memory>
+#include <string>
+#include <type_traits>
+#include <vector>
 using namespace std;
 using namespace std::string_literals;
+using namespace std::chrono_literals;
 
 int main(int argc, char* argv[])
 {

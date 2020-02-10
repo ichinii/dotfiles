e54d6f9fc43f11acd61fdde78975a7297b37efd5
Sven Erdem <sven.erdem@kdab.com>
2020-02-10 15:28:09 +0100

    try: add comment; cpp: include all headers

--- .try/cpp_src/main.cpp
+++ .try/cpp_src/main.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-#include <algorithm>
-#include <type_traits>
-#include <thread>
-#include <cstdint>
-#include <tuple>
+#include <bits/stdc++.h>
 
-int main(int argc, char* argv[])
+using namespace std;
+using namespace std::string_literals;
+using namespace std::chrono_literals;
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-	(void)argc;
-	(void)argv;
-
 	
 
 	return 0;
